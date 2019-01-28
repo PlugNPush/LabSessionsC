aa190c8223606c9d68da4fb8cff49d8e7a8992ef
PlugNCS <michael.nass@free.fr>
2019-01-28 08:28:54 +0100

    Initial Commit

---
+++ LabSessionsC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LabSessionsC
+//
+//  Created by PlugN on 28/01/2019.
+//  Copyright © 2019 Groupe MINASTE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
