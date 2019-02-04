1b3c4c7d24ec5dda1abf424e804e2fd989746fd7
PlugNCS <michael.nass@free.fr>
2019-02-04 08:33:16 +0100

    Configured for courses and TDs.

--- LabSessionsC/main.c
+++ LabSessionsC/main.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char * argv[]) {
+void TD1(){
     char str[20], rev[20];
     int i;
     printf("Enter a word : ");
@@ -19,7 +19,7 @@ int main(int argc, const char * argv[]) {
     }
     rev[i] = '\0';
     printf("The reverse of %s is %s\n", str, rev);
-   
+    
     char compare[20];
     printf("Enter another word : ");
     scanf("%s", compare);
@@ -29,6 +29,33 @@ int main(int argc, const char * argv[]) {
     } else {
         printf("The two words do not match...\n");
     }
-    
+    return;
+}
+
+void C1(){
+    return;
+}
+
+
+int main(int argc, const char * argv[]) {
+    int choice = 0;
+    printf("Main menu:\n 1 - TD1\n 2 - Course 1\n> ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 0:
+            printf("Something wrong happened.");
+            break;
+        case 1:
+            TD1();
+            break;
+        case 2:
+            C1();
+            break;
+            
+        default:
+            printf("Something wrong happened.");
+            break;
+    }
     return 0;
 }
+
