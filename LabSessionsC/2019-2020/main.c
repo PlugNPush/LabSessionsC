9f41a457805c1bb72f5f409fb89247f8f38dbbcc
PlugNCS <michael.nass@free.fr>
2020-02-15 16:10:36 +0100

    SVGD

--- LabSessionsC/2019-2020/main.c
+++ LabSessionsC/2019-2020/main.c
@@ -2,74 +2,76 @@
 //  main.c
 //  LabSessionsC
 //
-//  Created by PlugN on 30/01/2020.
+//  Created by PlugN on 13/02/2020.
 //  Copyright © 2020 Groupe MINASTE. All rights reserved.
 //
 
 #include "main.h"
 
-int moy3(int a, int b, int c) {
-    return (a+b+c)/3;
+int val(int t[], int taille, int value) {
+    for (int i=0; i<taille; i++) {
+        if (t[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int ord(int t[], int taille, int value) {
+    int i = 0;
+    while (t[i] < value && i < taille) {
+        i++;
+    }
+    return i;
+}
+
+int insert(int t[], int taille, int value) {
+    int block = ord(t, taille, value);
+    int i = 0;
+    int tempt[100];
+    for (i = 0; i < taille; i++) {
+        tempt[i] = t[i];
+    }
+    t[block] = value;
+    taille++;
+    for (i=block+1; i < taille; i++) {
+        t[i] = tempt[i-1];
+    }
+    return *t;
+}
+
+void main43() {
+    int t[100] = {2,4,5,6,7,9,13,35,47,57,78,97};
+    printf("[");
+    for (int i=0; i<13; i++) {
+        printf("%d, ", t[i]);
+    }
+    printf("]\n");
+    printf("Position de %d : %d\n", 13, val(t, 12, 13));
+    insert(t, 12, 3);
+    printf("Nouvelle position de %d : %d\n", 13, val(t, 13, 13));
+    
+    printf("[");
+    for (int i=0; i<13; i++) {
+        printf("%d, ", t[i]);
+    }
+    printf("]\n");
+}
+
+void main54() {
+    char test[1000];
+    int i = 0;
+    printf("Saisissez une chaîne");
+    scanf("%s", test);
+    for (i=0; test[i]; i++) {
+        printf("Le nombre de caractères est %d", i);
+    }
+    
 }
 
 int main(int argc, const char * argv[]) {
+    //printf("Hello world!\n");
+    main43();
     
-    int i;
-    i = 10;
     
-    if (i == 20 || 30) {
-        printf("True\n");
-    } else {
-        printf("False");
-    }
-    
-    switch (i) {
-        case 2:
-            printf("yo");
-            break;
-        case 4:
-            printf("Wow");
-        case 7:
-            printf("Unbelievable");
-        case 10:
-            printf("Amazing\n");
-        case 43:
-            printf("Uh oh\n");
-            
-        default:
-            break;
-    }
-    
-    int count;
-    count = 1;
-    
-    while (count <= 10) {
-        printf("%d\n", count);
-        count++;
-    }
-    
-    int r, j, k;
-    r = 3;
-    j = 2 * (r++);
-    // j = 6 et r = 4
-    k = 2 * (++r);
-    //
-    
-    printf("i = %d, j = %d, k = %d\n", r, j, k);
-    
-    for (i = 9; i > 10; i = i-2) {
-        printf("\n %d", i);
-    }
-    
-    i = 0;
-    
-    do {
-        --i;
-        printf("%d\n", i);
-        i++;
-    } while (i > 0);
-    
-    printf("%d\n", moy3(3, 6, 9));
-    
-    return 0;
 }
--- LabSessionsC/2019-2020/main1.h
+++ LabSessionsC/2019-2020/main1.h
@@ -2,12 +2,12 @@
 //  main.h
 //  LabSessionsC
 //
-//  Created by PlugN on 13/02/2020.
+//  Created by PlugN on 30/01/2020.
 //  Copyright © 2020 Groupe MINASTE. All rights reserved.
 //
 
-#ifndef main_h
-#define main_h
+#ifndef main1_h
+#define main1_h
 
 #include <stdio.h>
 #include <stdlib.h>
