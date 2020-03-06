2051d671780e7897e7fc764699ba7698b65c9d9b
PlugNCS <michael.nass@free.fr>
2020-03-06 13:44:33 +0100

    PUSH

--- LabSessionsC/2019-2020/main.c
+++ LabSessionsC/2019-2020/main.c
@@ -140,6 +140,27 @@ int main(int argc, const char * argv[]) {
             printf("]\n");
             
         }
+        else if (choice == 7) {
+            int A = 1;
+            int B = 2;
+            int C = 3;
+            int *p1, *p2;
+            p1 = &A;
+            p2 = &C;
+            *p1 = (*p2)++;
+            p1 = p2;
+            p2 = &B;
+            *p1 = *p2;
+            ++*p2;
+            *p1 *= *p2;
+            A =++*p2**p1;
+            p1 = &A;
+            *p2 = *p1 /= *p2;
+            
+            
+            int tab[5];
+            tab[0] = 10;
+        }
         
         
         else if (choice == 0) {
